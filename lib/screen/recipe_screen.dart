fad2667c134458d3f006ba43a665939eaddce6a6
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-01-01 19:13:07 +0700

    Feature: recipe for each meal

--- lib/screen/recipe_screen.dart
+++ lib/screen/recipe_screen.dart
@@ -12,12 +12,49 @@ class RecipeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Container(
-        alignment: Alignment.center,
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image.network(meal.imageUrl),
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Ingredients",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            const SizedBox(height: 10),
+            for (final ingredient in meal.ingredients.indexed)
+              Text(
+                "${ingredient.$1 + 1}. ${ingredient.$2}",
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            const SizedBox(height: 20),
+            Text(
+              "Steps",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            for (final step in meal.steps.indexed)
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  "Step ${step.$1 + 1}: ${step.$2}",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ),
           ],
         ),
       ),
